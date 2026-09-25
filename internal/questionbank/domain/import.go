a99e58e224bae4baaee568641cff59d313f847f7
nasef64 <nasef64@gmail.com>
2026-09-25 13:36:37 +0300

    feat(questionbank): define V2 import contracts

---
+++ internal/questionbank/domain/import.go
@@ -0,0 +1,63 @@
+package domain
+
+import "time"
+
+type ImportProvenance struct {
+	DocumentCode          string
+	SourceItemID          string
+	ImageHash             string
+	PDFPageIndex          int
+	PrintedPageNumber     int
+	PrintedQuestionNumber int
+}
+
+type ImportCommand struct {
+	Create     CreateCommand
+	Provenance ImportProvenance
+}
+
+type ImportConflict struct {
+	ID             string
+	QuestionCode   string
+	SourceItemID   string
+	ImageHash      string
+	ImportBatchID  string
+	WorkflowStatus WorkflowStatus
+}
+
+type ImportIssue struct {
+	Index        int    `json:"index"`
+	QuestionCode string `json:"questionCode"`
+	Code         string `json:"code"`
+	Message      string `json:"message"`
+}
+
+type ImportedQuestion struct {
+	ID             string
+	QuestionCode   string
+	WorkflowStatus WorkflowStatus
+}
+
+type ImportBatch struct {
+	BatchID        string
+	Status         string
+	RequestedCount int
+	InsertedCount  int
+	CreatedBy      string
+	CreatedAt      time.Time
+	RolledBackAt   *time.Time
+	Questions      []ImportedQuestion
+}
+
+type ImportResult struct {
+	Status        string
+	Mode          string
+	BatchID       string
+	Requested     int
+	Prepared      int
+	Inserted      int
+	QuestionCodes []string
+	Issues        []ImportIssue
+	Conflicts     []ImportConflict
+	Batch         *ImportBatch
+}
