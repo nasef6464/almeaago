456b18ed4e8d7f75d9a6e58cc614063a078f4339
nasef64 <nasef64@gmail.com>
2026-09-25 11:41:07 +0300

    refactor(organizations): keep parent facade off school router

--- internal/organizations/transport/http/handler.go
+++ internal/organizations/transport/http/handler.go
@@ -33,7 +33,6 @@ func New(service *orgapp.Service, auth Authenticator) http.Handler {
 
 	r.Get("/context", h.schoolContexts)
 	r.Get("/teacher-workspace", h.teacherWorkspace)
-	r.Get("/parent-authority", h.parentAuthority)
 	r.Get("/", h.listSchools)
 	r.Post("/", h.createSchool)
 	r.Get("/{schoolId}", h.schoolByID)
