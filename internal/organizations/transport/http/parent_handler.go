8c16fef048afc7e1de0a97f83b977d9b3cec8e1e
nasef64 <nasef64@gmail.com>
2026-09-25 11:41:19 +0300

    feat(organizations): add isolated parent authority facade

---
+++ internal/organizations/transport/http/parent_handler.go
@@ -0,0 +1,21 @@
+package organizationshttp
+
+import (
+	"net/http"
+
+	identityapp "github.com/nasef6464/almeaago/internal/identity/application"
+	orgapp "github.com/nasef6464/almeaago/internal/organizations/application"
+
+	"github.com/go-chi/chi/v5"
+)
+
+// NewParentFacade exposes only parent-authority routes. It intentionally does
+// not mount the school administration surface under /api/v1/parents.
+func NewParentFacade(service *orgapp.Service, auth Authenticator) http.Handler {
+	h := &Handler{service: service, auth: auth}
+	r := chi.NewRouter()
+	r.Get("/authority", h.parentAuthority)
+	return r
+}
+
+var _ identityapp.Authenticated
