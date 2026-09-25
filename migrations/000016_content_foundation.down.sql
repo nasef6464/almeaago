ffa58fd680b6fa841fdbf08aa0ab6cea524e8e89
nasef64 <nasef64@gmail.com>
2026-09-25 14:10:19 +0300

    feat(db): add content foundation rollback

---
+++ migrations/000016_content_foundation.down.sql
@@ -0,0 +1,19 @@
+BEGIN;
+
+DROP TABLE IF EXISTS topic_library_items;
+DROP TABLE IF EXISTS library_item_assets;
+DROP TABLE IF EXISTS library_skill_links;
+DROP TABLE IF EXISTS library_items;
+DROP TABLE IF EXISTS topic_lessons;
+DROP TABLE IF EXISTS topic_skill_links;
+DROP TABLE IF EXISTS foundation_topics;
+DROP TABLE IF EXISTS lesson_assets;
+DROP TABLE IF EXISTS lesson_skill_links;
+DROP TABLE IF EXISTS course_assets;
+DROP TABLE IF EXISTS course_skill_links;
+DROP TABLE IF EXISTS course_lessons;
+DROP TABLE IF EXISTS lessons;
+DROP TABLE IF EXISTS course_modules;
+DROP TABLE IF EXISTS courses;
+
+COMMIT;
