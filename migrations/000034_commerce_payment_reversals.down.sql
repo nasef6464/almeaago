7155af97924cf7b0e92e7bd07e452af4e74425d9
nasef64 <nasef64@gmail.com>
2026-09-26 19:09:58 +0300

    fix(commerce): restore provider session index on rollback

--- migrations/000034_commerce_payment_reversals.down.sql
+++ migrations/000034_commerce_payment_reversals.down.sql
@@ -9,6 +9,11 @@ ALTER TABLE commerce_revenue_entries
 
 DROP TABLE IF EXISTS commerce_payment_reversals;
 
+DROP INDEX IF EXISTS commerce_payment_requests_provider_session_idx;
+CREATE INDEX commerce_payment_requests_provider_session_idx
+  ON commerce_payment_requests(provider_code,provider_session_id)
+  WHERE provider_session_id <> '';
+
 ALTER TABLE commerce_provider_events
   DROP CONSTRAINT IF EXISTS commerce_provider_events_event_status_check;
 
